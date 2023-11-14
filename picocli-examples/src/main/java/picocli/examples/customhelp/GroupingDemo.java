36753d520c16a5ac574be41ff76eaa984b079acd
James Yoo <jyoo980@gmail.com>
2023-11-14 09:50:12 -0800

    Adding error suppression

--- picocli-examples/src/main/java/picocli/examples/customhelp/GroupingDemo.java
+++ picocli-examples/src/main/java/picocli/examples/customhelp/GroupingDemo.java
@@ -110,6 +110,7 @@ class CommandGroupRenderer implements CommandLine.IHelpSectionRenderer {
         return textTable;
     }
 
+    @SuppressWarnings("method.invocation") // application-invariant : subcommands is populated before this call.
     private int maxLength(Map<String, CommandLine> subcommands, int max) {
         int result = subcommands.values().stream().map(cmd -> cmd.getCommandSpec().names().toString().length() - 2).max(Integer::compareTo).get();
         return Math.min(max, result);
