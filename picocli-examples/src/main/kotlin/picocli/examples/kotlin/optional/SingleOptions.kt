67431203f9b9b80fbcb57ce8c169c358ab447836
James Yoo <jyoo980@gmail.com>
2023-12-01 22:01:03 -0800

    Adding SpotBugs annotations

--- picocli-examples/src/main/kotlin/picocli/examples/kotlin/optional/SingleOptions.kt
+++ picocli-examples/src/main/kotlin/picocli/examples/kotlin/optional/SingleOptions.kt
@@ -2,10 +2,12 @@ package picocli.examples.kotlin.optional
 
 import picocli.CommandLine.Option
 import picocli.CommandLine.Parameters
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import java.util.*
 
 class SingleOptions {
     @Option(names = ["-x"])
+    @SuppressFBWarnings(value="NP_OPTIONAL_RETURN_NULL", justification="Suppression of Warning")
     lateinit var x: Optional<Int>
 
     @Option(names = ["-y"])
